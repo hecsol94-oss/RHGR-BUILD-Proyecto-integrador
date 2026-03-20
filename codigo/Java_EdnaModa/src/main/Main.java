0c7a54217a36edc158ce8a8eeb0ce5ce03232b54
Hector Solis <hecsol4@gmail.com>
2026-03-20 17:43:43 +0100

    Proyecto integrador en Java eclipse (modificacion de la ventana vista)

---
+++ codigo/Java_EdnaModa/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import vista.*;
+
+public class Main {
+	
+	public static void main(String []args) {
+		InicioSesion sesion = new InicioSesion();
+	    sesion.setVisible(true);
+	}
+	
+	
+
+}
---
+++ codigo/Java_EdnaModa/src/vista/VentanaAprendiz.java
@@ -0,0 +1,72 @@
+package vista;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SpringLayout;
+
+public class VentanaAprendiz extends JFrame {
+	
+	public VentanaAprendiz() {
+		SpringLayout springLayout = new SpringLayout();
+		getContentPane().setLayout(springLayout);
+		
+		JMenuBar menuBar = new JMenuBar();
+		springLayout.putConstraint(SpringLayout.NORTH, menuBar, 0, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, menuBar, 138, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, menuBar, 285, SpringLayout.WEST, getContentPane());
+		
+		JMenu Citas = new JMenu("Citas");
+		menuBar.add(Citas);
+		
+		JMenuItem VerCitas = new JMenuItem("Ver citas");
+		Citas.add(VerCitas);
+		VerCitas.setEnabled(false);
+		
+		JMenuItem reservarCitas = new JMenuItem("reservar citas");
+		Citas.add(reservarCitas);
+		reservarCitas.setEnabled(false);
+
+		JMenuItem misCitas = new JMenuItem("Mis citas");
+		Citas.add(misCitas);
+		
+		JMenu Talleres = new JMenu("Talleres");
+		menuBar.add(Talleres);
+		Talleres.setEnabled(false);
+		
+		JMenuItem misTalleres = new JMenuItem("Mis talleres");
+		Talleres.add(misTalleres);
+		
+		JMenu verTalleres = new JMenu("verTalleres");
+		Talleres.add(verTalleres);
+		
+		JMenuItem nuevoTaller = new JMenuItem("Nuevo Taller");
+		Talleres.add(nuevoTaller);
+		
+		JMenu Clientes = new JMenu("Clientes");
+		menuBar.add(Clientes);
+		Clientes.setEnabled(false);
+		
+		JMenuItem verClientes = new JMenuItem("verClientes");
+		Clientes.add(verClientes);
+		
+		JMenuItem misClientes = new JMenuItem("Mis clientes");
+		Clientes.add(misClientes);
+		
+		JMenuItem nuevoCliente = new JMenuItem("Nuevo cliente");
+		Clientes.add(nuevoCliente);
+		getContentPane().add(menuBar);
+		
+		JLabel Imagen = new JLabel("");
+		Imagen.setForeground(new Color(255, 255, 255));
+		Imagen.setIcon(new ImageIcon(InicioSesion.class.getResource("/img/RGHR_(1).png")));
+		Imagen.setBounds(0, 0, 4000, 1000);
+		getContentPane().add(Imagen);
+	}
+	
+}
